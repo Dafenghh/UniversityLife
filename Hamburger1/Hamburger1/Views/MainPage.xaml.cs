851ad8196e22390e5a79e0e2831fdc26475726f9
延泽 许 <peter13994@126.com>
2018-05-29 22:17:25 +0800

    初步设计

---
+++ Hamburger1/Hamburger1/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Hamburger1.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Hamburger1.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
